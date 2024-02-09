e5e49232f886a58f7c5a46ab27e8c5e3933d50ce
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2024-02-09 15:27:52 +0300

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,61 @@
 #!/usr/bin/python3
 """
-Python script takes URL from stdin and compute exact metrics
+Write a script that reads stdin line by line and computes metrics
 """
+import sys
+
+"""
+Dictionary to store the number of lines for each status code
+"""
+code_status = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0}
+
+# Variable to store the total file size
+total_sizes = 0
+# Counter to keep track of the number of lines read
+count_line = 1
 
 
-def printst(dictionary, size):
+def printStats():
     """
-    Print function
+    Print the final statistics after all lines have been read
     """
-    d = sorted(dictionary.keys())
-    print("File size: {:d}".format(size))
-    for i in d:
-        if dictionary[i] != 0:
-            print("{}: {:d}".format(i, dictionary[i]))
+    print('File size: {}'.format(total_sizes))
+    for code in sorted(code_status.keys()):
+        if code_status[code] != 0:
+            print('{}: {}'.format(code, code_status[code]))
 
-count = 0
-size = 0
-dic = {"200": 0, "301": 0, "400": 0, "401": 0,
-       "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    with open(0) as f:
-        for line in f:
-            count += 1
-            arr = line.split()
-            try:
-                size += int(arr[-1])
-            except:
-                pass
-            try:
-                st = arr[-2]
-                if st in dic:
-                    dic[st] += 1
-            except:
-                pass
-            if count % 10 == 0:
-                printst(dic, size)
-        printst(dic, size)
+    # Read lines from stdin one by one
+    for line in sys.stdin:
+        try:
+            line = line[:-1]
+            parts = line.split(' ')
+            total_sizes += int(parts[-1])
+            status_code = int(parts[-2])
+            if status_code in code_status:
+                code_status[status_code] += 1
+        except Exception:
+            # Skip the line if it is not in the expected format
+            pass
+
+        """
+        Print the statistics every 10 lines or
+        when a keyboard interruption occurs
+        """
+        if count_line % 10 == 0:
+            printStats()
+        count_line += 1
 
 except KeyboardInterrupt:
-    printst(dic, size)
+    printStats()
     raise
+printStats()
