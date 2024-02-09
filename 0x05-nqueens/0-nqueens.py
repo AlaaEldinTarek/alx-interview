81633b6ec3d42154f12dbb08792e31e54bc78c82
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2024-02-09 14:57:26 +0300

    done

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,68 +4,90 @@ Module for 0x0C. N Queens.
 Holberton School
 Specializations - Interview Preparation ― Algorithms
 """
-from sys import argv, exit
+import numpy as np
+import itertools
+from functools import partial
 
+BLOCKED = 0
+FREE = 1
+OCCUPIED = 2
 
-def solveNQueens(n):
-    """Program that solves the N queens problem"""
-    res = []
-    queens = [-1] * n
-    # queens is a one-dimension array, like [1, 3, 0, 2] means
-    # index represents row no and value represents col no
+class NQueens:
+    def __init__(self, board_size):
+        self.board_size = board_size
+        self.board = np.full((board_size, board_size), FREE, dtype=int)
+        self.solutions = 0
 
-    def dfs(index):
-        """Recursively resolves the N queens problem"""
-        if index == len(queens):  # n queens have been placed correctly
-            res.append(queens[:])
-            return  # backtracking
-        for i in range(len(queens)):
-            queens[index] = i
-            if valid(index):  # pruning
-                dfs(index + 1)
+    def is_valid_coord(self, coordinate):
+        row, column = coordinate
+        return 0 <= row < self.board_size and 0 <= column < self.board_size
 
-    # check whether nth queens can be placed
-    def valid(n):
-        """Method that checks if a position in the board is valid"""
-        for i in range(n):
-            if abs(queens[i] - queens[n]) == n - i:  # same diagonal
-                return False
-            if queens[i] == queens[n]:  # same column
-                return False
-        return True
+    def block_positions(self, row, column):
+        blocked_positions = set()
 
-    # given queens = [1,3,0,2] this function returns
-    # [[0, 1], [1, 3], [2, 0], [3, 2]]
+        # Block left
+        for col in range(column - 1, -1, -1):
+            blocked_positions.add((row, col))
 
-    def make_all_boards(res):
-        """Method that builts the List that be returned"""
-        actual_boards = []
-        for queens in res:
-            board = []
-            for row, col in enumerate(queens):
-                board.append([row, col])
-            actual_boards.append(board)
-        return actual_boards
+        # Block right
+        for col in range(column + 1, self.board_size):
+            blocked_positions.add((row, col))
 
-    dfs(0)
-    return make_all_boards(res)
+        # Block up
+        for rw in range(row - 1, -1, -1):
+            blocked_positions.add((rw, column))
 
+        # Block down
+        for rw in range(row + 1, self.board_size):
+            blocked_positions.add((rw, column))
+
+        # Block L up-diag
+        rw, col = row, column
+        while self.is_valid_coord((rw - 1, col - 1)):
+            rw -= 1
+            col -= 1
+            blocked_positions.add((rw, col))
+
+        # Block L down-diag
+        rw, col = row, column
+        while self.is_valid_coord((rw + 1, col - 1)):
+            rw += 1
+            col -= 1
+            blocked_positions.add((rw, col))
+
+        # Block R up-diag
+        rw, col = row, column
+        while self.is_valid_coord((rw - 1, col + 1)):
+            rw -= 1
+            col += 1
+            blocked_positions.add((rw, col))
+
+        # Block R down-diag
+        rw, col = row, column
+        while self.is_valid_coord((rw + 1, col + 1)):
+            rw += 1
+            col += 1
+            blocked_positions.add((rw, col))
+
+        return blocked_positions
+
+    def place_queens(self, row=0):
+        if row == self.board_size:
+            self.solutions += 1
+            print(self.board)
+            print()
+            return
+
+        for col in range(self.board_size):
+            if self.board[row, col] == FREE and row not in self.block_positions(row, col):
+                self.board[row, col] = OCCUPIED
+                self.place_queens(row + 1)
+                self.board[row, col] = FREE
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        print('Usage: nqueens N')
-        exit(1)
-    try:
-        n = int(argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
-
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
-    else:
-        result = solveNQueens(n)
-        for row in result:
-            print(row)
-            
+    board_size = 5
+    n_queens = NQueens(board_size)
+    n_queens.place_queens()
+    print(f"{n_queens.solutions} solutions")
+    print("Finished")
+    
