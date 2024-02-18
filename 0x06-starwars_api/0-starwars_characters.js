7f9f73eb7365466569ffbcf63283f72a4d8e6ab6
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2024-02-18 13:01:24 +0300

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const request = require("request");
+import { get } from "request";
 const url =
   "https://swapi-api.alx-tools.com/api/films/" + process.argv[2] + "/?format=json";
-request.get(url, function (response, body) {
+get(url, function (response, body) {
   const film = JSON.parse(body.body);
   // debugging
   // console.log(film);
@@ -11,7 +11,7 @@ request.get(url, function (response, body) {
 });
 const character = (people, i = 0) => {
   if (i === people.length) return;
-  request.get(people[i], function (response, body) {
+  get(people[i], function (response, body) {
     // console.log(people[i]);
     console.log(JSON.parse(body.body).name);
     character(people, i + 1);
